1fc41d0acc4cd5359d9aaa50e9eee41a666663e9
Susan Cagle <susyjustice@gmail.com>
2020-10-11 11:06:32 -0400

    Delete API.js
    This has my private key information and I was getting emails so I had to delete.

--- client/src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const BASEURL = "https://www.googleapis.com/books/v1/volumes?q=";
-const APIKEY = "&key=AIzaSyDZQyd2wzg_0BH3t8FSsCWOvHnsnssTdHc";
-
-export default {
-  search: function (query) {
-    return axios.get(BASEURL + query + APIKEY);
-  },
-};
