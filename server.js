53e96e8d558b14b37fcc8e9d4f3fe2c588864393
Rocco Losito <roccolosito@gmail.com>
2020-10-17 11:16:52 -0400

    modified server js

--- server.js
+++ server.js
@@ -15,13 +15,8 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
-  { 
-    useNewUrlParser: true, 
-    useCreateIndex: true, 
-    useUnifiedTopology: true, 
-    useFindAndModify: false
-});
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
 
 // Start the API server
 app.listen(PORT, function() {
