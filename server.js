2c68cd68a5d7a2aa465b329d67e5000028bc6e20
Rocco Losito <roccolosito@gmail.com>
2020-10-16 21:30:17 -0400

    removed dupe in gitignore file

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks",
+  { 
+    useNewUrlParser: true, 
+    useCreateIndex: true, 
+    useUnifiedTopology: true, 
+    useFindAndModify: false
+});
 
 // Start the API server
 app.listen(PORT, function() {
