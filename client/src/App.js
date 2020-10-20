1c9ec6d0bedf57abe2144d4906edae8917e4663c
Rocco Losito <roccolosito@gmail.com>
2020-10-20 13:32:40 -0400

    fresh install of create react app

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import "./App.css";
-// import API from "./utils/API";
+// import "./App.css";
 import Header from "./components/Header/Header";
 import SearchPage from "./pages/Search";
 import SavedPage from "./pages/Saved";
